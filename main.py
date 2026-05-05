09d1e7f3f00dd3ab85bd6834e948e468034fe869
clairelee05 <160359323+clairelee05@users.noreply.github.com>
2026-05-04 19:59:41 -0700

    breaking things

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import os
+import sys
+
 def main():
-    print("Hello from dsc190-assignment5!")
+    unused_variable = "this should break"
+    print("Hello from dsc190-assignment5!")   
+    print(  "extra spaces break")
+    return None
 
 
 if __name__ == "__main__":
