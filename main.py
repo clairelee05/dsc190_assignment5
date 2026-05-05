40f1256ec699484bd294357e6694cad0eefefbce
clairelee05 <160359323+clairelee05@users.noreply.github.com>
2026-05-04 20:02:44 -0700

    fixed broken things

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import os
-import sys
+def main(): 
+    print("Hello from dsc190-assignment5!") 
 
-def main():
-    unused_variable = "this should break"
-    print("Hello from dsc190-assignment5!")   
-    print(  "extra spaces break")
-    return None
-
-
-if __name__ == "__main__":
+if __name__ == "__main__": 
     main()
